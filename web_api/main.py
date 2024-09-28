f413bae068dc1cacbdb1ce7b73480871cc8e077b
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-09-28 14:00:35 -0400

    update main web app file clean it

--- web_api/main.py
+++ web_api/main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 SECRET_KEY = "a_very_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -28,8 +28,26 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-hashed_admin_password = get_password_hash("admin")
+def get_db_connection():
+    conn = sqlite3.connect('../ram_data.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise Exception
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return username
+
+hashed_admin_password = get_password_hash("admin")
 
 @app.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -46,30 +64,8 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise Exception
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return username
-def get_db_connection():
-    conn = sqlite3.connect('../ram_data.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
 @app.get("/ram_data/")
-async def protected_data(current_user: str = Depends(get_current_user), n: int):
+async def protected_data(n: int ,current_user: str = Depends(get_current_user)):
     if n <= 0:
         raise HTTPException(status_code=400, detail="n must be greater than 0")
 
